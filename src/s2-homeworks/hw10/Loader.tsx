5afa01c7765cb14d4a94db89b43363c048c1c4db
Danil <baba_ejik@mail.ru>
2023-08-25 16:17:58 +0600

    дом h12

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,5 +1,10 @@
 import s from './Loader.module.css'
 
 export const Loader = () => {
-    return <div className={s.loader}/>
+    return <div className={s.preloader}>
+        <div className={s.preloader__row}>
+            <div className={s.preloader__item}></div>
+            <div className={s.preloader__item}></div>
+        </div>
+    </div>
 }
